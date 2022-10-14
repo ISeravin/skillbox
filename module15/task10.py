c75be64c2e6ebb270cde8dc49b3dae847cb01d6f
ISeravin <i_seravin955@mail.ru>
2022-10-14 12:54:03 +0300

    Update task10.py

--- module15/task10.py
+++ module15/task10.py
@@ -1,10 +1,19 @@
 # Задача 10. Сортировка
 
-lst = [1, 4, -3, 0, 10]
-print("Изначальный список:", lst)
-#print("Отсортированный список:", sorted(lst))
+def lst_imput():
+    list = []
+    for i in range(int(input('Количество цифр в списке: '))):
+        list.append(int(input(f'Введите {i+1} число: ')))
+    return list
 
+# lst = [1, 4, -3, 0, 10]
+lst = lst_imput()
+print("Изначальный список:", lst)
+# print("Отсортированный список:", sorted(lst))
 for i in range(len(lst)):
-    if lst[i::] > lst[i + 1::]:
-        lst[i + 1::] = lst[i::]
+    for current in range(i, len(lst)):
+        if lst[i] > lst[current]:
+            lst[current], lst[i] = lst[i], lst[current]
+            
 print("Отсортированный список:", lst)
+
