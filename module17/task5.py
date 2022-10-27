c20ad6c176ff8a85ea816af104a685491d40675a
ISeravin <i_seravin955@mail.ru>
2022-10-27 11:44:57 +0300

    Update task5.py

--- module17/task5.py
+++ module17/task5.py
@@ -1,3 +1,5 @@
 #Задача 5. Разворот
 str_input = input("Введите строку: ")
+str_input = str_input[str_input.find('h') + 1:str_input.rfind('h')]
 print("Развёрнутая последовательность между первым и последним h: ", end = '')
+print(str_input[::-1])
